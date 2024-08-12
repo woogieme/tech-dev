e570cab585359dcd6bce44c1374c377b8f1b106c
woogieme <wodnr8462@naver.com>
2024-08-12 21:00:05 +0900

    fix: Password 암호화(without Security)

---
+++ src/main/java/org/c4marathon/corn/PasswordEncoder.java
@@ -0,0 +1,33 @@
+package org.c4marathon.corn;
+
+
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+@Component
+public class PasswordEncoder {
+
+    public static String encryptPassword(String password) {
+        try {
+            SecureRandom random = new SecureRandom();
+            byte[] salt = new byte[16];
+            random.nextBytes(salt);
+
+            KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 85319, 128);
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("OPerationSH$14");
+
+            byte[] hash = factory.generateSecret(spec).getEncoded();
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
